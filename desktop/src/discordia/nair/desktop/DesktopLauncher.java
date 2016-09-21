b97bdf893c69feaa3552a8bd29dffb36106e1d26
dalud <jaakko.niska@gmail.com>
2016-09-21 12:51:17 +0300

    Initial commit

---
+++ desktop/src/discordia/nair/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package discordia.nair.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import discordia.nair.NairMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NairMain(), config);
+	}
+}
